224db19bd3b540966f28f5bb1b3cb4ed45fb7447
isaaccamacho54 <icamachoichaparro@gmail.com>
2024-11-21 19:04:01 -0600

    mi programa en JS

---
+++ 20082024/app1.js
@@ -0,0 +1,11 @@
+let nombre = prompt("Ingresa tu nombre ");
+console.log(nombre);
+
+let edad = prompt("Ingresa tu edad");
+let nacionalidad = prompt("Ingresa tu nacionalidad");
+
+console.log("Tu nombre es: " + nombre);
+console.log("Tu edad es: " + edad);
+console.log("Tu nacionalidad es: " + nacionalidad);
+
+console.log(typeof resultado);
